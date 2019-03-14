3ca5016e396bf0e06d0b7e581c4fec706caaf7d6
Sean OLoughlin <27134812+SeanoNET@users.noreply.github.com>
2019-03-14 21:05:16 +0800

    Added configuration load to example

--- NetoDotNET.Examples/Program.cs
+++ NetoDotNET.Examples/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace NetoDotNET.Examples
 {
@@ -6,7 +8,17 @@ namespace NetoDotNET.Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Load configuration
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+
+            var config = configBuilder.Build();
+
+            Console.WriteLine($"Loaded Configuration:" + Environment.NewLine +
+                $"Neto Store Name: {config.GetSection("NETO_STORENAME").Value}" + Environment.NewLine +
+                $"Neto API Key: {config.GetSection("NETO_API_KEY").Value}" + Environment.NewLine +
+                $"Neto Username: {config.GetSection("NETO_USERNAME").Value}");
         }
     }
 }
